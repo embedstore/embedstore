f1f683a8035704bd09e3bb8c120ff2eb9ccd0ec8
Taranjeet Singh <reachtotj@gmail.com>
2023-05-14 08:27:14 +0530

    add info api

--- pyembeddingtown/__init__.py
+++ pyembeddingtown/__init__.py
@@ -1,6 +1,13 @@
 
-from pyembeddingtown.apis import get_embeddings_api
+from pyembeddingtown.apis import (
+    get_embeddings_api,
+    get_all_embeddings_info,
+)
 
 
 def get_embeddings(doc_id):
     return get_embeddings_api(doc_id)
+
+
+def get_embeddings_info():
+    return get_all_embeddings_info()
--- pyembeddingtown/apis.py
+++ pyembeddingtown/apis.py
@@ -6,6 +6,7 @@ import tempfile
 API_BASE_URL = "https://embeddingtown.com/"
 
 API_ENDPOINT_GET_EMBEDDING_DOC = "api/embedding/v1/download/"
+API_ENDPOINT_GET_ALL_EMBEDDING_INFO = "api/embedding/v1/info/"
 
 
 def get_embeddings_api(doc_id):
@@ -31,3 +32,12 @@ def get_embeddings_api(doc_id):
         return embeddings
     else:
         raise Exception('Failed to fetch embeddings')
+
+
+def get_all_embeddings_info():
+    url = f"{API_BASE_URL}{API_ENDPOINT_GET_ALL_EMBEDDING_INFO}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Failed to fetch embeddings info")
